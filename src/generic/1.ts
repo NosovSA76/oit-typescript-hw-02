eb82ee619823866d94f2b0482cf8b177aff58726
NosovSA76 <125302593+NosovSA76@users.noreply.github.com>
2023-11-09 05:41:21 +0200

    Change

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -2,14 +2,13 @@
   Є функція getPromise(), яка повертає проміс, що дозволяється в масив, що містить рядки та числа. 
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
-
-function getPromise<T>(): Promise<T> {
+function getPromise(): Promise<[string, number]> {
   return new Promise((resolve) => {
-    resolve(['Text', 50] as T);
+    resolve(['Text', 50] );
   });
 }
 
-getPromise<[string, number]>()
+getPromise()
 .then((data) => {
   console.log(data);
 });
